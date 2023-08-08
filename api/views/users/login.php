d6ae4808afebe587bd4d55cb3710a598f0a18f8a
krisikostadinov <krisi.199898@gmail.com>
2023-08-08 20:51:05 +0300

    Add login

---
+++ api/views/users/login.php
@@ -0,0 +1,48 @@
+<?php
+
+$response = new Response();
+global $db;
+
+if (isset($_POST) && $_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  $jsonData = file_get_contents('php://input');
+  $data = json_decode($jsonData, true);
+
+  if (isset($data["email"]) == FALSE || isset($data["password"]) == FALSE) {
+    $response->sendError("all_fields_is_required", 400);
+  }
+
+  $email = htmlspecialchars($data["email"]);
+  $password = htmlspecialchars($data["password"]);
+
+  if (empty($email) || empty($password)) {
+    $response->sendError("all_fields_is_required", 400);
+  }
+
+  if (filter_var($email, FILTER_VALIDATE_EMAIL) == FALSE) {
+    $response->sendError("invalid_email", 400);
+  }
+
+  $params = array(":email" => $email);
+  $user = $db->select("SELECT * FROM users WHERE email = :email;", $params);
+
+  if ($user == FALSE) {
+    $response->sendError("email_is_not_found", 404);
+  }
+
+  $user = $user[0];
+
+  $krisi_jwt = new KrisiJWT(SECRET_JWT);
+  $payload = [
+    "id" => $user["id"],
+    "password" => $user["password"]
+  ];
+  $expiration = time() + 3600; // 1 hour from now
+  $salt = bin2hex(random_bytes(32)); // Генерира сол от 32 байта (256 бита)
+
+  $token = $krisi_jwt->encode($payload, $expiration, $salt);
+
+  $response->setData(array("token" => $token));
+  $response->setStatusCode(201);
+  $response->sendJson();
+}

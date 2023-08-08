85feacc57d2537982799fd920e32c02e14f21f69
krisikostadinov <krisi.199898@gmail.com>
2023-08-08 12:49:18 +0300

    Initial commit

---
+++ client/src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-title> Online Quiz </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "MainLayout",
+});
+</script>
---
+++ client/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Online Quiz"
+      src="~assets/logo.jpeg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
